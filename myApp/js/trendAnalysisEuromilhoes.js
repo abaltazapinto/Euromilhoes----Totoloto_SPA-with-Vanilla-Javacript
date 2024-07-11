96232e71f43498f442b2bd0659ab5f221dc6173f
abaltazapinto <andrebaltazarpinto@gmail.com>
2024-07-11 21:17:01 +0100

    jhkgh

--- myApp/js/trendAnalysisEuromilhoes.js
+++ myApp/js/trendAnalysisEuromilhoes.js
@@ -117,6 +117,8 @@ let stars = [
     [6,8],// 9th july 2024
   ];
 
+
+
 // Count the occurrences of each number over time
 const countOccurrencesOverTime = (data) => {
     let countsOverTime = {};
@@ -219,3 +221,57 @@ const performTrendAnalysis = () => {
 
 // Call the function to perform trend analysis
 performTrendAnalysis();
+
+
+//D3// 
+
+
+// Flatten the array of arrays into a single array of numbers
+const allNumbers = historicalData.flat();
+
+// Set the dimensions and margins of the graph
+const margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// Append the svg object to the body of the page
+const svg = d3.select("#euromilhoesHistogramChart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// X axis: scale and draw
+const x = d3.scaleLinear()
+    .domain([0, 50])     // can be changed according to your data
+    .range([0, width]);
+svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+// Set the parameters for the histogram
+const histogram = d3.histogram()
+    .value(d => d)
+    .domain(x.domain())
+    .thresholds(x.ticks(50));
+
+// Apply the function to data to get the bins
+const bins = histogram(allNumbers);
+
+// Y axis: scale and draw
+const y = d3.scaleLinear()
+    .range([height, 0]);
+y.domain([0, d3.max(bins, d => d.length)]);   // d3.hist has to be called before the Y axis obviously
+svg.append("g")
+    .call(d3.axisLeft(y));
+
+// Append the bar rectangles to the svg element
+svg.selectAll("rect")
+    .data(bins)
+  .enter().append("rect")
+    .attr("x", 1)
+    .attr("transform", d => "translate(" + x(d.x0) + "," + y(d.length) + ")")
+    .attr("width", d => x(d.x1) - x(d.x0) - 1)
+    .attr("height", d => height - y(d.length))
+    .style("fill", "#69b3a2");
